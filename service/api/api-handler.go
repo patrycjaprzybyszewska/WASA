fabcb9f939818482834968c2ff761f0c15245e85
patrycjaprzybyszewska <patprzyb456@gmail.com>
2024-12-03 11:07:57 +0100

    go

---
+++ service/api/api-handler.go
@@ -0,0 +1,16 @@
+package api
+
+
+import (
+	"net/http"
+)
+
+func (rt *_router) Handler() http. Handler {
+	rt.router.POST("/message", rt.sendMessage)
+	rt.router.DELETE("/message/:id", rt.deleteMessage)
+	rt.routrt.PUT("/message/:id", rt.commentMessage)
+	rt.router.POST("/message/:id", rt.forwardMessage)
+	rt.router.DELETE("/message/comment/:id", rt.deleteComment)
+	rt.router.POST("/session", rt.doLogin)
+	rt.router.PUT("session/:id/userName", rt.setMyUsername)
+}
