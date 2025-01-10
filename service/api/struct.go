d614bc18eaf19360b81a7b0edd52a8019f381e10
patrycjaprzybyszewska <patprzyb456@gmail.com>
2025-01-10 16:23:54 +0100

    123

--- service/api/struct.go
+++ service/api/struct.go
@@ -6,21 +6,21 @@ import (
 )
 
 
-type User struct {
+type User struct{
 	UserId 		int64 	`json:"userID"`
 	UserName 	int	`json:"name"`
 	UserPhoto 	string 	`json:"userPhoto"`
 }
 
 
-func (u *User) FromDatabase(user database.User)
-{
+func (u *User) FromDatabase(user database.User){
 	u.UserId = user.UserId
 	u.UserName = user.UserName
+	u.UserPhoto = user.UserPhoto
 }
 
 func (u *User) ToDatabase() database.User{
-	retutn database.User{
+	return database.User{
 		UserId: u.UserId,
 		UserPhoto: u.UserPhoto,
 		UserName: u.UserName,
