564d7f94b50069956de1aa37dbae7ecde6e50c94
patrycjaprzybyszewska <patprzyb456@gmail.com>
2025-01-10 16:10:32 +0100

    123

--- service/database/user-database.go
+++ service/database/user-database.go
@@ -1,18 +1,13 @@
 package database
 
-import (
-	"database/sql"
-)
 
-func (db *appdbimpl) CreateUser(u User) (User, error) {
-	res, err := db.c.Exec("INSERT INTO users(name, userphoto) VALUES (?,?)", u.Username)
-	if err != nil {
-		var user User
-	}
+func (db *appdbimpl) CreateLogin(u User) (User, error) {
+	res, err := db.c.Exec("INSERT INTO users(name, userphoto) VALUES (?,?)", u.UserName)
+
 	lastInsertID, err := res.LastInsertId()
 	if err != nil {
 		return u, err
 	}
-	u.UserIdId = uint64(lastInsertID)
+	u.UserId = uint64(lastInsertID)
 	return u, nil
 }
