3db29d858b96ee8d253d2c4c878e731749c58aaf
patrycjaprzybyszewska <patprzyb456@gmail.com>
2025-01-13 22:49:26 +0100

    11

--- service/api/message.go
+++ service/api/message.go
@@ -2,7 +2,7 @@ package api
 
 
 import (
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
+	//"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
 	"github.com/julienschmidt/httprouter"
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
 	"net/http"
