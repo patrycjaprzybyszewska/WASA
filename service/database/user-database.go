811579267e0960e430121be89e6818eb106b85fa
patrycjaprzybyszewska <patprzyb456@gmail.com>
2025-01-15 03:40:35 +0100

    typo

--- service/database/user-database.go
+++ service/database/user-database.go
@@ -67,9 +67,9 @@ func (db *appdbimpl) SetUserphoto(u User, photo string) (User, error) {
 	return u, nil
 }
 
-func (db *appdbimpl) GetUserById(userId uint64) error {
-	var user User 
-    err := db.c.QueryRow(`SELECT * FROM users WHERE userId = ?`, userId).Scan(&user) 
+func (db *appdbimpl) GetUserById(authId uint64) error {
+	var user  User 
+    err := db.c.QueryRow(`SELECT * FROM users WHERE userId = ?`, authId).Scan(&user) 
     if err != nil {
         return err
     }
