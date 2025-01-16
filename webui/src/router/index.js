a01c0a649db41d6588879a9a3c35babeeceee774
patrycjaprzybyszewska <patprzyb456@gmail.com>
2025-01-16 19:59:20 +0100

    login

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,6 +1,5 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
