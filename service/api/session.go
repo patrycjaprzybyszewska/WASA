d06544d041222ccb4797345deacf1910478b0d14
patrycjaprzybyszewska <patprzyb456@gmail.com>
2025-01-16 21:01:57 +0100

    fizes

--- service/api/session.go
+++ service/api/session.go
@@ -54,7 +54,7 @@ func (rt *_router) setMyUserName(w http.ResponseWriter, r *http.Request, ps http
 		return
 	}
 	
-	user.UserPhoto, err = rt.db.GetUserPhotoById(userId)
+	user.UserPhoto, err = rt.db.GetUserPhotoById(user.UserId)
 	
 	dbuser, err := rt.db.SetUsername(user.ToDatabase(), user.UserName)
 	if err != nil {
