25258bcea7811cc66098f7ef8c106e49ca6dae28
patrycjaprzybyszewska <patprzyb456@gmail.com>
2025-01-17 14:50:53 +0100

    cors

--- cmd/webapi/cors.go
+++ cmd/webapi/cors.go
@@ -11,11 +11,13 @@ import (
 func applyCORSHandler(h http.Handler) http.Handler {
 	return handlers.CORS(
 		handlers.AllowedHeaders([]string{
-			"x-example-header",
+			"content-type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "X-Requested-With", "Authorization",
 		}),
 		handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "DELETE", "PUT"}),
-		// Do not modify the CORS origin and max age, they are used in the evaluation.
 		handlers.AllowedOrigins([]string{"*"}),
-		handlers.MaxAge(1),
+		handlers.ExposedHeaders([]string{"Authorization"}),
+		handlers.AllowedHeaders([]string{"Authorization", "Content-Type", "searcher_id",
+			"identifier", "requester_ID", "user_id", "user_name", "commenter_name"})
 	)(h)
 }
+	// Some of the allowed headers are legacy.
