1f8928e628d3ea7760156dd6ff0a2d1c6329d1f4
patrycjaprzybyszewska <patprzyb456@gmail.com>
2025-01-19 14:47:26 +0100

    now?

--- webui/src/views/ChatsView.vue
+++ webui/src/views/ChatsView.vue
@@ -13,6 +13,7 @@ export default {
             selectedChat: null,
             MessagetoForward: null,
             chattoforwardId: null,
+            successmsg: null, 
     };
   },
   created() {
