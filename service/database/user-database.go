0cfc81917474952a1d0c23b88456c316391e042c
patrycjaprzybyszewska <patprzyb456@gmail.com>
2025-01-21 00:32:49 +0100

    username

--- service/database/user-database.go
+++ service/database/user-database.go
@@ -42,8 +42,8 @@ func (db *appdbimpl) GetUserNameById(userId uint64) (string, error) {
 }
 func (db *appdbimpl) SetUsername(u User, username string) (User, error) {
 	var userId uint64
-	err := db.c.QueryRow(`SELECT userName FROM users WHERE userName = ?`, username).Scan(&userId)
-	if err == nil{
+	err := db.c.QueryRow(`SELECT userId FROM users WHERE userName = ?`, username).Scan(&userId)
+	if userId != nil{
 		  return u, fmt.Errorf("UserName is not aviable, cannot be changed")}
 	
 	res, err := db.c.Exec(`UPDATE users SET userName=?, userPhoto=? WHERE userId=?`, username, u.UserPhoto, u.UserId)
