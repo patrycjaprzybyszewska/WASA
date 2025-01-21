b093cf984065132f41924c42ac0ab4ea104bab2f
patrycjaprzybyszewska <patprzyb456@gmail.com>
2025-01-21 02:27:11 +0100

    chat

--- service/database/messages-database.go
+++ service/database/messages-database.go
@@ -11,15 +11,17 @@ func (db *appdbimpl) Sendmessage(m Message) (Message, error) {
 	res, err := db.c.Exec(`INSERT INTO messages (senderId, chatId, content, messageDate, messageTime, state) 
                         VALUES (?, ?, ?, ?, ?, ?)`, m.SenderId, m.ChatId, m.Content, m.MessageDate, m.MessageTime, m.State)
 	
-	_, err = db.c.Exec("INSERT INTO chat_users (chatId, userId) VALUES (?, ?)", .ChatId, m.SenderId)
-	if err != nil {
-				return fmt.Errorf("error adding user to chat: %w", err)
-		}
+	
 
 	lastInsertID, err := res.LastInsertId()
 	if err != nil {
 		return m, fmt.Errorf("error fetching last insert ID: %w", err)
 	}
+
+	_, err = db.c.Exec("INSERT INTO chat_users (chatId, userId) VALUES (?, ?)", .ChatId, m.SenderId)
+	if err != nil {
+				return m, fmt.Errorf("error adding user to chat: %w", err)
+		}
 
 
 	return m, nil
