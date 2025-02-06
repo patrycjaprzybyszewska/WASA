d21d432e052f1d2311b92fd429d7d6601036ea60
patrycjaprzybyszewska <patprzyb456@gmail.com>
2025-02-06 15:08:50 +0100

    commma

--- service/database/database.go
+++ service/database/database.go
@@ -55,7 +55,7 @@ type Comment struct {
 	CommentId uint64 `json:"commentId"`
 	MessageId uint64 `json:"messageId"`
 	Content   string `json:"content"`
-	Commentername string `json:"commentername`
+	Commentername string `json:"commentername"`
 }
 
 type Chat struct {
